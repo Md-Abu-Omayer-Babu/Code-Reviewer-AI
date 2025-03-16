f18f36d354bcc9d1541fe4f2bd844310dbabcef7
Md Abu Omayer Babu <abuomayerbabu@gmail.com>
2025-03-16 16:31:38 +0600

    added dragable and dropable components

---
+++ frontend learning/drag_drop/src/app/box/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Box from "../components/Box";
+
+function page() {
+  return (
+    <Box />
+  );
+}
+
+export default page;
---
+++ frontend learning/drag_drop/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid bg-amber-100 grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl font-bold">Drag and drop</h1>
+      <main className="flex flex-row cursor-pointer gap-[32px] row-start-2 items-center sm:items-start">
+        HomePage
+      </main>
+    </div>
+  );
+}
